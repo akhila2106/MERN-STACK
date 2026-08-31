82c81c19845e7ae93b4a84bc64c84d73d4146fc9
akhila2106 <yakhila247224p@gmail.com>
2026-08-31 12:13:27 +0530

    update

---
+++ 06-folder/myapp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-500'>
+      <h1 className='bg-pink-300'> Welcome to party</h1>
+    </div>
+  )
+}
+
+export default App
